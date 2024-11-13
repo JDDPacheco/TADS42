c28091c9ab18c7dedccc371b0eefc24d8b368d68
jddpacheco <josediogo.dp@gmail.com>
2024-11-12 20:50:45 -0400

    Implementado botão clear no aplicativo de cadastro de produto

--- PDM/ProductRegister/app/src/main/java/com/example/productregister/MainActivity.java
+++ PDM/ProductRegister/app/src/main/java/com/example/productregister/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.productregister;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextDescription;
+    private EditText editTextPrice;
+    private EditText editTextIncommingQty;
+    private EditText editTextDate;
+    private Spinner spinnerSupplier;
+    private RadioGroup radioGroupOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editTextDescription = findViewById(R.id.editTextDescription);
+        editTextPrice = findViewById(R.id.editTextPrice);
+        editTextIncommingQty = findViewById(R.id.editTextIncommingQty);
+        editTextDate = findViewById(R.id.editTextDate);
+        spinnerSupplier = findViewById(R.id.spinnerSupplier);
+        radioGroupOptions = findViewById(R.id.radioGroupOptions);
     }
+
+    public void clearOnClick(View view){
+        editTextDescription.setText("");
+        editTextPrice.setText("");
+        editTextIncommingQty.setText("");
+        editTextDate.setText("");
+        spinnerSupplier.setSelection(0);
+        radioGroupOptions.clearCheck();
+    }
+
 }
