77c9f0ce4aa89349ed866a525ddac4d861fa6824
jddpacheco <josediogo.dp@gmail.com>
2025-01-02 22:19:45 -0400

    Finalizada aula de DRA

---
+++ DRA/projects/aplicacao-dra2024/src/test/java/edu/ifam/dra/aplicacao_dra2024/repository/CidadeRepositoryTest.java
@@ -0,0 +1,65 @@
+package edu.ifam.dra.aplicacao_dra2024.repository;
+
+import edu.ifam.dra.aplicacao_dra2024.model.Cidade;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DataJpaTest
+public class CidadeRepositoryTest {
+
+    @Autowired
+    private CidadeRepository cidadeRepository;
+
+    @Test
+    public void deveSalvarCidadeValida(){
+
+        Cidade cidade = new Cidade();
+        cidade.setNome("Manaus");
+        cidade.setEstado("AM");
+
+        Cidade cidadeSalva = cidadeRepository.save(cidade);
+
+        assertThat(cidadeSalva.getId()).isNotNull();
+        assertThat(cidadeSalva.getNome()).isEqualTo("Manaus");
+        assertThat(cidadeSalva.getEstado()).isEqualTo("AM");
+
+    }
+
+    @Test
+    public void naoDeveSalvarCidadeComNomeNulo(){
+
+        Cidade cidade = new Cidade();
+        cidade.setNome(null);
+        cidade.setEstado("AM");
+
+        assertThrows(DataIntegrityViolationException.class, () -> {
+            cidadeRepository.save(cidade);
+        });
+
+    }
+
+    @Test
+    public void naoInserirComEstadoEmBranco(){
+
+        Cidade cidade = new Cidade();
+        cidade.setNome("Manaus");
+        cidade.setEstado(null);
+
+        Cidade cidadeSalva = cidadeRepository.save(cidade);
+
+        assertThat(cidadeSalva.getId()).isNotNull();
+        assertThat(cidadeSalva.getNome()).isEqualTo("Manaus");
+        assertThat(cidadeSalva.getEstado()).isEqualTo("AM");
+
+        //assertThrows(DataIntegrityViolationException.class, () -> {
+        //    cidadeRepository.save(cidade);
+        //});
+
+    }
+
+}
